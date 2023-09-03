3933b154582dd0e7b6fb5e6ccdfcce0fdcd13bd4
dustinaxman <daxman@u.rochester.edu>
2023-09-02 21:53:39 -0700

    code refactoring for inference

--- src/chaserner/inference/utils.py
+++ src/chaserner/inference/utils.py
@@ -9,31 +9,31 @@ import time
 #import os
 #os.environ["OMP_NUM_THREADS"] = "1"
 
-def input_text_list_to_extracted_entities(input_text_list, config_path):
-    start_time_1 = time.time()
+
+def load_model(config_path):
     config_path = Path(config_path)
     with open(config_path) as f:
         config = json.load(f)
     ids2lbl = {v: k for k, v in config["lbl2ids"].items()}
     max_length = config["max_length"]
-    model_path = config_path.parent/config["best_checkpoint"]
+    model_path = config_path.parent / config["best_checkpoint"]
     tokenizer_name = config["tokenizer_name"]
     tokenizer = BertTokenizerFast.from_pretrained(tokenizer_name)
     # TODO: remove the extra args here later!!! for later models
     if "torchscript_model" in config:
-        model_path = config_path.parent/config["torchscript_model"]
+        model_path = config_path.parent / config["torchscript_model"]
         print("LOADING TORCHSCRIPT FILE")
         model = torch.jit.load(str(model_path))
     else:
         print("LOADING MODEL CHECKPOINT")
-        model = NERModel.load_from_checkpoint(checkpoint_path=model_path, hf_model_name=tokenizer_name, label_to_id=config["lbl2ids"])
+        model = NERModel.load_from_checkpoint(checkpoint_path=model_path, hf_model_name=tokenizer_name,
+                                              label_to_id=config["lbl2ids"])
     model.eval()
     model = model.to('cpu')
+    return model, tokenizer, max_length, ids2lbl
 
-    #max_input_length = max([len(txt.split()) for txt in input_text_list])
 
-    start_time_2 = time.time()
-
+def run_ner_model(input_text_list, model, tokenizer, max_length, ids2lbl):
     token_lengths = [len(tokenizer.tokenize(txt)) for txt in input_text_list]
 
     # Find the maximum token length from the tokenized texts
@@ -52,8 +52,25 @@ def input_text_list_to_extracted_entities(input_text_list, config_path):
     ).to("cpu")
 
     outputs = model(tokenized_data["input_ids"], tokenized_data["attention_mask"])
-    #print(outputs)
+    # print(outputs)
     labels_list = model_output_to_label_tensor(outputs, tokenized_data["offset_mapping"], ids2lbl)
+    entity_extracted_samples = [{"input_text": input_text,
+                                 "extracted_entities": {k: v for v, k in extract_entities(input_text.split(), labels)}}
+                                for input_text, labels in zip(input_text_list, labels_list)]
+    return entity_extracted_samples
+
+
+def input_text_list_to_extracted_entities(input_text_list, config_path):
+    start_time_1 = time.time()
+    config_path = Path(config_path)
+    model, tokenizer, max_length, ids2lbl = load_model(config_path)
+
+    #max_input_length = max([len(txt.split()) for txt in input_text_list])
+
+    start_time_2 = time.time()
+
+    entity_extracted_samples = run_ner_model(input_text_list, model, tokenizer, max_length, ids2lbl)
+
     total_time = time.time() - start_time_2
     load_time = start_time_2 - start_time_1
     per_utt_time = total_time/float(len(input_text_list))
@@ -61,6 +78,6 @@ def input_text_list_to_extracted_entities(input_text_list, config_path):
     # for input_text, labels in zip(input_text_list, labels_list):
     #     print(input_text)
     #     print(labels)
-    return [{"input_text": input_text, "extracted_entities": {k: v for v, k in extract_entities(input_text.split(), labels)}} for input_text, labels in zip(input_text_list, labels_list)]
+    return entity_extracted_samples
 
 
